8e95b240cc2ba7be4d5afe229d614192bff912eb
Amalia <amalialorenaferariu@gmail.com>
2021-10-29 16:22:34 +0200

    palindroma passi logici

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,4 +1,11 @@
 // Palidroma:
 // Chiedere all’utente di inserire una parola
+    //Creare un prompt
+    //Salvare la risposta in una variabile x
 // Creare una funzione per capire se la parola inserita è palindroma.
+    //Trasformare il valore della variabile x in un array -> .split('') salvandolo in una nuova variabile
+    //Usare il metodo reverse() per invertire l'ordine degli elementi al suo interno.
+    //Trasformare l'array invertito in una stringa -> .join('')
+        //Salviamo il valore in una variabile y
+    //Confrontare la variabile x con la variabile y 
 // Dare l’output relativo.
